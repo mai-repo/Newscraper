21a8735c356fe4cebb2028231dabaa46e822820f
mai-repo <ttmai@my.loyno.edu>
2025-01-21 00:08:50 -0500

    add Tailwind CSS setup and update project files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export const content = [
+  './templates/**/*.html',
+  './static/**/*.js',
+  './**/*.py',
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
