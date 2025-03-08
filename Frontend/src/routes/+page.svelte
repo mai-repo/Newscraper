080c0a1bfec39a1df7e5835c438cc77b36544965
Mai <60695595+mai-repo@users.noreply.github.com>
2025-03-08 03:12:06 -0500

    Update verification state to false and modify reCAPTCHA script loading

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -8,7 +8,7 @@
     import { message } from './components/store.ts';
 
     // Initialize verification state
-    let verification = true;
+    let verification = false;
 
     // Function to handle reCAPTCHA verification in parent
     function handleVerification(success) {
--- Frontend/src/routes/components/Captacha.svelte
+++ Frontend/src/routes/components/Captacha.svelte
@@ -34,7 +34,7 @@
 <section>
     <!-- Google reCAPTCHA widget -->
     <!-- Include Google reCAPTCHA API -->
-    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <div class="mb-3 g-recaptcha" data-sitekey="6Le24-wqAAAAAI4lL6lz0kE-zg_5keKRI-nTyXwb"></div>
     <button class = "p-2 bg-blue-300 rounded-lg" type="submit" on:click={notARoboto}>Submit</button>
 </section>
